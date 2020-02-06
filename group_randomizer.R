87a6315dd35d96f858c5fb164c752a3b69afac4a
michaelnelso <michaelfrancenelson@gmail.com>
2020-02-06 17:25:29 -0500

    autosaving

--- group_randomizer.R
+++ group_randomizer.R
@@ -1,3 +1,28 @@
+
+random_groups = function(names, n_groups, group_name_prefix = "Group ", grouping = "grouping 1", seed = NA)
+{
+  if (!is.na(seed)) set.seed(seed)
+  
+  require(data.table)
+  suppressWarnings(
+    {
+      tmp1 = split(sample(names), 1:n_groups)
+    }
+  )
+  
+  vvv = vector(mode = "character", length = 0)
+  out = data.table(name = vvv, group = vvv, grouping = vvv)
+  
+  names(tmp1) = paste0(group_name_prefix, 1:n_groups)
+  for (i in 1:length(tmp1))
+    out =  rbind(data.table(name = as.character(tmp1[[i]]), group = as.character(names(tmp1[i])), grouping = grouping), out)
+  
+  out = out[order(out$group, out$name), ]
+  
+  return(out)
+}
+
+
 dat = read.csv("roster_feb_4.csv")
 head(dat)
 nrow(dat)
@@ -5,26 +30,5 @@ names = as.character(sample(dat$FullName))
 names_1 = strsplit(names, ",")
 names_final = sapply(names_1, function(x) paste(x[2], x[1]))
 
-seq(1, 7)
-
-seq(1, length(names_final), length.out = 8)
-
-
-(index_start = seq(1, 37, len = 7))
-(index_end   = c(head(index_start, -1) + 5, 43))
-(indices = data.frame(start = index_start, end = index_end, size = index_end - index_start + 1))
-
-sum(indices$size)
-
-
-groups_string = c()
-
-for (i in 1:nrow(indices))
-{
-groups_string = c(groups_string, paste0("Group ", i), names_final[indices[i, 1]:indices[i, 2]])  
-}
-
-groups_string
-
-clipr::write_clip(groups_string)
+random_groups(names_final, 5, "Bear Group ", "Bear Groups", seed = 1)
 
