df175fe9a0e1f54987f2e71e9e0e12bf3e8b1d3a
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-02-25 01:04:49 -0500

    unit 6 part 1

--- data/environment_vars.R
+++ data/environment_vars.R
@@ -1,3 +1,4 @@
+require(here)
 colorize <- function(x, color) {
   if (knitr::is_latex_output()) {
     sprintf("\\textcolor{%s}{%s}", color, x)
@@ -15,6 +16,9 @@ bold_col <- function(x, color) {
 }
 
 
+slide_img_dir = "slides/slide_images"
+chris_img_dir = "Images"
+chris_dat_dir = "data/chris_data"
 # book_data = "/data/Fletcher_Fortin-2018-Supporting_Files/data/"
 
 # Store data locally:
